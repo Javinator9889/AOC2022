3169493176d208449e628673c1e1b9ac9c171125
Javinator9889 <dev@javinator9889.com>
2022-12-03 20:36:11 +0100

    Template: introduce part 2

--- template.py
+++ template.py
@@ -5,24 +5,43 @@ from __future__ import annotations
 import sys
 import typing
 
+from enum import Enum
 from pathlib import Path
 
 if typing.TYPE_CHECKING:
     ...
 
 
-def advent(input: str) -> str:
+class Part(Enum):
+    P1 = "P1"
+    P2 = "P2"
+
+
+def advent_p1(input: str) -> str:
+    raise NotImplementedError("This method must be overridden")
+
+
+def advent_p2(input: str) -> str:
     raise NotImplementedError("This method must be overridden")
 
 
 if __name__ == "__main__":
-    assert len(sys.argv) == 2, f"usage: {sys.argv[0]} INPUT_FILE"
+    assert len(sys.argv) in {2, 3}, f"usage: {sys.argv[0]} INPUT_FILE [P1/P2]"
     file = Path(sys.argv[1])
+    part = Part(sys.argv[2]) if len(sys.argv) == 3 else Part.P1
 
     assert file.exists(), f'input file "{file}" does not exist'
     assert file.is_file(), f'input file "{file}" is not a file'
 
     with file.open() as fd:
-        print(advent(fd.read()))
+        match part:
+            case Part.P1:
+                fn = advent_p1
+            case Part.P2:
+                fn = advent_p2
+            case _:
+                raise SystemError(f"Impossible pattern {part}")
+
+        print(fn(fd.read()))
 
     sys.exit(0)
